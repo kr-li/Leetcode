770114753d2d97ebf34e1a86408907091837b640
kr_li <55921715+kr-li@users.noreply.github.com>
2020-11-16 22:24:37 +0800

    offer18

---
+++ Offer/src/Offer17.java
@@ -0,0 +1,14 @@
+public class Offer17 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] printNumbers(int n) {
+        int len = (int)Math.pow(10, n) - 1;
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++){
+            ans[i] = i + 1;
+        }
+        return ans;
+    }
+}
---
+++ Offer/src/Offer18.java
@@ -0,0 +1,22 @@
+public class Offer18 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode item = new ListNode(0);
+        ListNode dummy = item;
+        item.next = head;
+        while (item.next.val != val){
+            item = item.next;
+        }
+        item.next = item.next.next;
+        return dummy.next;
+    }
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
