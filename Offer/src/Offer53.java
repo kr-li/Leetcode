704fa53bf5f10af51e950ed5f9db813d17aae0fe
kr_li <55921715+kr-li@users.noreply.github.com>
2021-01-26 21:51:35 +0800

    update

--- Offer/src/Offer53.java
+++ Offer/src/Offer53.java
@@ -4,21 +4,55 @@ public class Offer53 {
     }
 
     public int search(int[] nums, int target) {
+        int leftK = findFirstK(nums, target);
+        int rightK = findLastK(nums, target);
+        if (leftK == -1 && rightK == -1) return 0;
+        return rightK - leftK + 1;
+    }
+
+    private int findFirstK(int[] nums, int target){
         int left = 0;
         int right = nums.length - 1;
-        int ans = 0;
         while (left <= right){
             int mid = (left + right) >> 1;
-            if (mid == target){
-                ans++;
+            if (nums[mid] == target){
+                if (mid > 0 && nums[mid - 1] == target){
+                    right = mid - 1;
+                }
+                else {
+                    return mid;
+                }
             }
-            if (target < nums[mid]){
-                right = mid - 1;
-            }
-            else {
+            else if (nums[mid] < target){
                 left = mid + 1;
             }
+            else {
+                right = mid - 1;
+            }
         }
-        return ans;
+        return -1;
+    }
+
+    private int findLastK(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target){
+                if (mid < nums.length - 1 && nums[mid + 1] == target){
+                    left = mid + 1;
+                }
+                else {
+                    return mid;
+                }
+            }
+            else if (nums[mid] < target){
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
